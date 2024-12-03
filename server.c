781fb9688cd2294a549da78bcc073af75df63e8d
cnimcheski <cnimcheski@hotmail.com>
2024-12-03 14:45:08 -0700

    finished server.c

--- server.c
+++ server.c
@@ -31,21 +31,72 @@ int main() {
 	while (1) {
 		// TODO:
 		// read requests from serverFIFO
-
-
-
-
-
-
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
 		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
+		// read requests from serverFIFO
+		if (read(server, &req, sizeof(req)) > 0) {
+			printf("Received a request from %s to send the message '%s' to %s.\n", 
+			       req.source, req.msg, req.target);
+
+			// TODO:
+			// open target FIFO and write the whole message struct to the target FIFO
+			// Construct the target FIFO name
+			char targetFIFO[100];
+			snprintf(targetFIFO, sizeof(targetFIFO), "%sFIFO", req.target);
+
+			// Open target FIFO for writing
+			target = open(targetFIFO, O_WRONLY);
+			if (target == -1) {
+				perror("Error opening target FIFO");
+				continue; // Skip to the next message
+			}
+
+			// Write the message to the target FIFO
+			if (write(target, &req, sizeof(req)) == -1) {
+				perror("Error writing to target FIFO");
+			}
+
+			// TODO:
+			// close target FIFO after writing the message
+			close(target);
+		} else {
+			perror("Error reading from serverFIFO");
+			break; // Exit the loop on read error
+		}
 
 
 
 
+		// if (read(server, &req, sizeof(req)) > 0) {
+		// 	printf("Received a request from %s to send the message '%s' to %s.\n", 
+		// 	       req.source, req.msg, req.target);
+
+		// 	// Construct the target FIFO name
+		// 	char targetFIFO[100];
+		// 	snprintf(targetFIFO, sizeof(targetFIFO), "%sFIFO", req.target);
+
+		// 	printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
+
+		// 	// TODO:
+		// 	// open target FIFO and write the whole message struct to the target FIFO
+		// 	// close target FIFO after writing the message
+		// 	// Open target FIFO for writing
+		// 	target = open(targetFIFO, O_WRONLY);
+		// 	if (target == -1) {
+		// 		perror("Error opening target FIFO");
+		// 		continue; // Skip to the next message
+		// 	}
+
+		// 	// Write the message to the target FIFO
+		// 	if (write(target, &req, sizeof(req)) == -1) {
+		// 		perror("Error writing to target FIFO");
+		// 	}
+
+		// 	// Close the target FIFO after writing
+		// 	close(target);
+		// } else {
+		// 	perror("Error reading from serverFIFO");
+		// 	break; // Exit the loop on read error
+		// }
 
 
 
